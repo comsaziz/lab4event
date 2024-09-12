2eb9461b2a638e5fa0baa0d24de2e920002f00e7
comsaziz <comsaziz@gmail.com>
2024-09-12 14:26:27 +0300

    add lab4

---
+++ main.js
@@ -0,0 +1,30 @@
+let stop = document.getElementById('stopButton')
+let slow = document.getElementById('slowButton')
+let go = document.getElementById('goButton')
+let stopLight = document.getElementById('stopLight')
+let slowLight = document.getElementById('slowLight')
+let goLight = document.getElementById('goLight')
+
+
+stop.addEventListener("click", ()=>{
+    stopLight.style.backgroundColor = "red"
+    slowLight.style.backgroundColor = "black"
+    goLight.style.backgroundColor = "black"
+    text.textContent = stopLight.value
+
+})
+
+slow.addEventListener("click", ()=>{
+    slowLight.style.backgroundColor = "yellow"
+  stopLight.style.backgroundColor = "black"
+    goLight.style.backgroundColor = "black"
+    text.textContent = slowLight.value
+
+})
+go.addEventListener("click", ()=>{
+    stopLight.style.backgroundColor = "black"
+    slowLight.style.backgroundColor = "black"
+    goLight.style.backgroundColor = "green"
+    text.textContent = goLight.value
+
+})
